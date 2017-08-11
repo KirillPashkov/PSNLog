6f6122e980de444228849a53de886c8f9b293111
Kirill Pashkov <KirillPashkov@yandex.ru>
2017-08-11 10:13:01 +0300

    Add files via upload

---
+++ PK-Log.psm1
@@ -0,0 +1,54 @@
+﻿#[void][System.Reflection.Assembly]::LoadFile("$PSHOME\Modules\PK-NLog\lib\NLog.dll")
+[void][System.Reflection.Assembly]::LoadFile("$env:USERPROFILE\Documents\WindowsPowerShell\Modules\PK-Log\lib\NLog.dll")
+
+function Get-NewLogger {
+param( 
+[Parameter(Mandatory=$true)] 
+[String]$loggerName 
+) 
+[NLog.LogManager]::GetLogger($loggerName) 
+}
+
+function Get-NewLogConfig {
+New-Object NLog.Config.LoggingConfiguration 
+}
+
+function Get-NewLogTarget {
+param( 
+[Parameter(Mandatory=$true)] 
+[ValidateSet('console','file','mail')]
+[String]$targetType 
+) 
+	switch ($targetType) {
+		"console" {
+			New-Object NLog.Targets.ColoredConsoleTarget	
+		}
+		"file" {
+			New-Object NLog.Targets.FileTarget
+		}
+		"mail" { 
+			New-Object NLog.Targets.MailTarget
+		}
+	}
+}
+
+function Get-LogMessageLayout {
+param(
+[Parameter(Mandatory=$true)] 
+[Int]$layoutId 
+ ) 
+	switch ($layoutId) {
+		1 {
+			$layout	= '${longdate} | ${machinename} | ${processid} | ${processname} | ${level} | ${logger} | ${message}'
+		}
+		default {
+			$layout	= '${longdate} | ${machinename} | ${processid} | ${processname} | ${level} | ${logger} | ${message}'
+		}
+	}
+	return $layout
+}
+
+Export-ModuleMember -Function Get-NewLogger,
+                              Get-NewLogConfig,
+                              Get-NewLogTarget,
+                              Get-LogMessageLayout
---
+++ Test-Script.ps1
@@ -0,0 +1,84 @@
+﻿Import-Module PK-Log
+
+function Test-ScriptLogger {
+	
+	# Create a log config
+	$logCfg						= Get-NewLogConfig
+	
+	# Configure file logging target
+	$debugLog 					= Get-NewLogTarget -targetType "file"
+	$debugLog.ArchiveAboveSize 	= 102400
+	$debugLog.archiveEvery 		= "Month"
+	$debugLog.ArchiveNumbering 	= "Rolling"	
+	$debugLog.CreateDirs		= $true	
+	$debugLog.FileName 			= 'C:\log\debug.log'
+	$debugLog.Encoding 			= [System.Text.Encoding]::GetEncoding("UTF-8")
+	$debugLog.KeepFileOpen 		= $false
+	$debugLog.Layout 			= Get-LogMessageLayout -layoutId 1	
+	$debugLog.maxArchiveFiles 	= 1
+	
+	# Add target to Log config
+	$logCfg.AddTarget("file", $debugLog)
+	
+	# Configure a console logging target
+	$console 					= Get-NewLogTarget -targetType "console"
+	$console.Layout 			= Get-LogMessageLayout -layoutId 1
+	
+	# Add target to Log config
+	$logCfg.AddTarget("console", $console)
+	
+	# Configure a mail logging target
+	#$mailLog					= Get-NewLogTarget -targetType "mail"
+	#$mailLog.SmtpServer		= "smtp.mynet.local"
+	#$mailLog.From				= "scriptlogger@mynet.local"
+	#$mailLog.To				= "receiver@mail.me"
+	#$mailLog.Encoding 			= [System.Text.Encoding]::GetEncoding("UTF-8")
+	#$mailLog.Subject			= "[NLog] MailLogger Test"
+	#$mailLog.Layout			= Get-LogMessageLayout -layoutId 1
+	
+	
+	
+	<# 
+	[NLog.Targets.MailTarget]$testTarget = $null
+	
+	$testTarget.From
+	$testTarget.To
+	$testTarget.SmtpServer
+	$testTarget.Layout
+	$testTarget.Body
+	$testTarget.Name
+	$testTarget.Encoding
+	$testTarget.Subject
+	#>
+	
+	
+	# Configure rules for targets
+	$rule1 = New-Object NLog.Config.LoggingRule("*", [NLog.LogLevel]::Trace, $console)
+	$logCfg.LoggingRules.Add($rule1)
+	
+	$rule2 = New-Object NLog.Config.LoggingRule("*", [NLog.LogLevel]::Debug, $debugLog)
+	$logCfg.LoggingRules.Add($rule2)
+
+	# Rule to send only mail for Error messages
+	#$rule3 = New-Object NLog.Config.LoggingRule("*", [NLog.LogLevel]::Error, $mailLog)
+	#$logCfg.LoggingRules.Add($rule3)
+	
+	# Assign configured Log config to LogManager
+	[NLog.LogManager]::Configuration = $logCfg
+	
+	# Create a new Logger
+	$Log = Get-NewLogger -loggerName "TestLogger"
+	
+	# Write test Log messages
+	$Log.Debug("Debug Message")
+	$Log.Info("Info Message")
+	$Log.Warn("Warn Message")
+	$Log.Error("Error Message")
+	$Log.Trace("Trace Message")
+	$Log.Fatal("Fatal Message")
+	
+	$Log.Error("Error Message")
+	$Log.Error("Error Message")
+}
+
+Test-ScriptLogger
